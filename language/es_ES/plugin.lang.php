5707cf8fe4c0a359b3be9e2237ccf9dd8cdc54a6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-19 06:51:20 +0000

    [reply_to] Update es_ES, thanks to : petaqui
    git-svn-id: http://piwigo.org/svn/extensions/reply_to@15114 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -20,5 +20,5 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['reply to this comment'] = 'contestar a este comentario';
+$lang['Reply'] = 'Responder';
 ?>
